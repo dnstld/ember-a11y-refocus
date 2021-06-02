01e72e48c42918063026e0a20569ec30f38d3806
Melanie Sumner <melaniersumner@gmail.com>
2021-06-02 14:38:46 -0500

    app maintenance

--- tests/integration/components/navigation-narrator-test.js
+++ tests/integration/components/navigation-narrator-test.js
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | navigation-narrator', function(hooks) {
+module('Integration | Component | navigation-narrator', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('navigation message renders', async function(assert) {
-    await render(hbs`{{navigation-narrator}}`);
-    assert.dom(this.element).hasText(
+  test('navigation message renders', async function (assert) {
+    await render(hbs`<NavigationNarrator/>`);
+    assert.equal(
+      this.element.textContent.trim(),
       'The page navigation is complete. You may now navigate the page content as you wish.'
     );
-
   });
 });
